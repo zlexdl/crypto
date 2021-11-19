70f4a941dd4fcdb8aedafc85939adef228635df1
lei.zhang <zlexdl@163.com>
2021-11-19 22:34:33 +0800

    add HoppyMeme

--- getTwitter.py
+++ getTwitter.py
@@ -48,7 +48,7 @@ def send_mail(tweet):
     message['To'] = "6585852@qq.com, 393899161@qq.com"
     subject = tweet.user.screen_name
     message['Subject'] = Header(subject, 'utf-8')
-    text = MIMEText(tweet.text)
+    text = MIMEText(tweet.text) # .extended_tweet['full_text']
 
     message.attach(text)
     media = tweet.entities.get('media', [])
@@ -84,13 +84,13 @@ while True:
         # public_tweets = api.home_timeline(count=50)
         # public_tweets = api.list_timeline(list_id=1413685290284163076,count=50)
 
-        # zlexdl = api.user_timeline(screen_name='zlexdl', count=3)
-        CryptoFaibik = api.user_timeline(screen_name='CryptoFaibik', count=3)
-        ElemonGame = api.user_timeline(screen_name='ElemonGame', count=3)
-        top7ico = api.user_timeline(screen_name='top7ico', count=3)
-        ZssBecker = api.user_timeline(screen_name='ZssBecker', count=3)
-        HoppyMeme = api.user_timeline(screen_name='HoppyMeme', count=3)
-        Aleph__Zero = api.user_timeline(screen_name='Aleph__Zero', count=3)
+        # zlexdl = api.user_timeline(screen_name='zlexdl', count=3, tweet_mode='extended')
+        CryptoFaibik = api.user_timeline(screen_name='CryptoFaibik', count=3, tweet_mode='extended')
+        ElemonGame = api.user_timeline(screen_name='ElemonGame', count=3, tweet_mode='extended')
+        top7ico = api.user_timeline(screen_name='top7ico', count=3, tweet_mode='extended')
+        ZssBecker = api.user_timeline(screen_name='ZssBecker', count=3, tweet_mode='extended')
+        HoppyMeme = api.user_timeline(screen_name='HoppyMeme', count=3, tweet_mode='extended')
+        Aleph__Zero = api.user_timeline(screen_name='Aleph__Zero', count=3, tweet_mode='extended')
         print("---------------------public_tweets")
         public_tweets = CryptoFaibik + ElemonGame + top7ico + ZssBecker + HoppyMeme + Aleph__Zero
 
@@ -110,11 +110,11 @@ while True:
         logging.info("id=" + str(tweet.id))
         logging.info("created_at=" + str(tweet.created_at))
         logging.info("tweet---------------------")
-        logging.info(tweet.text)
-        logging.info("tweet---------------------")
+        logging.info(tweet.full_text)
+        logging.info("tweet---------------------"+ tweet.full_text)
         logging.info("screen_name=" + tweet.user.screen_name)
         utcTime_minutes = datetime.utcnow() - timedelta(minutes=5)
-        print(tweet.text)
+        print(tweet.full_text)
         print("---------------------")
         print(tweet.created_at.replace(tzinfo=None))
         print(utcTime_minutes.replace(microsecond=0))
@@ -124,11 +124,11 @@ while True:
             # send_mail(tweet)
             tran = ''
             try:
-                tran = ts.google(tweet.text, from_language='en', to_language='zh-CN')
+                tran = ts.google(tweet.full_text, from_language='en', to_language='zh-CN')
             except Exception as e:
                 tran = ''
                 print(str(e))
-            text = tweet.text + '\n\n-----------------------\n\n' + tran
+            text = tweet.full_text + '\n\n-----------------------\n\n' + tran
             send_pushplus(tweet.user.screen_name, text, 'TW001')
 
     print("time.sleep(300) start time=" + str(datetime.utcnow()))
